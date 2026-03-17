dd633370c89dfb52cd3691ca71837c052c26d02f
Yesopi <y.pineros@uniandes.edu.co>
2026-03-17 18:16:37 -0500

    remove Google Services plugin references and simplify Firebase initialization in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
