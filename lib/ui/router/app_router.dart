6ca6ea362b07b44869b3ab1e3a83916b6da654d4
mariamartinez44 <mc.martinezm1@uniandes.edu.com>
2026-03-18 22:22:06 -0500

    app router update: signup

--- lib/ui/router/app_router.dart
+++ lib/ui/router/app_router.dart
@@ -2,15 +2,21 @@ import 'package:go_router/go_router.dart';
 
 import '../home/home_view.dart';
 import '../login/login_view.dart';
+import '../signup/signup_view.dart';
+
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: '/login',
+    initialLocation: '/signup',
 
     routes: [
       GoRoute(
         path: '/login',
         builder: (context, state) => LoginPage(),
+      ),
+      GoRoute(
+        path: '/signup',
+        builder: (context, state) => SignUpPage(),
       ),
       GoRoute(
         path: '/',
