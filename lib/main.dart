051493a62751d4d8f0e015cd1c26fbfced655537
mariamartinez44 <mc.martinezm1@uniandes.edu.com>
2026-03-19 16:57:44 -0500

    notifier de connectivity en main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 import 'package:marketplace_flutter_application/ui/home/home_viewmodel.dart';
 import 'package:marketplace_flutter_application/ui/login/login_model.dart';
 import 'package:marketplace_flutter_application/ui/signup/signup_model.dart';
+import 'package:marketplace_flutter_application/ui/connectivity/connectivity_model.dart';
+import 'package:marketplace_flutter_application/data/services/connectivity_service.dart';
 import 'ui/router/app_router.dart';
 
 void main() {
@@ -19,6 +21,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => LoginModel()),
         ChangeNotifierProvider(create: (_) => SignUpModel()),
         ChangeNotifierProvider(create: (_) => HomeViewModel()),
+        ChangeNotifierProvider(create: (_) => ConnectivityModel(ConnectivityService()), child: MyApp(), );
       ],
       child: MaterialApp.router(
         title: 'Uniandes Marketplace',
@@ -28,3 +31,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
