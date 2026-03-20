0b51035e30b5acbe34b2e9c90eeeaa61e895bb9d
mariamartinez44 <mc.martinezm1@uniandes.edu.com>
2026-03-20 17:52:20 -0500

    router

--- lib/ui/router/app_router.dart
+++ lib/ui/router/app_router.dart
@@ -7,7 +7,7 @@ import '../signup/signup_view.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: '/',
+    initialLocation: '/signup',
 
     routes: [
       GoRoute(
