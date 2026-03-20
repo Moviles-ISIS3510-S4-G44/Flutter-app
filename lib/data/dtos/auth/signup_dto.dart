71cec056aa48765ad69e69a815ea44167c7c683c
mariamartinez44 <mc.martinezm1@uniandes.edu.com>
2026-03-20 11:06:53 -0500

    estructura dtos + dto signup

---
+++ lib/data/dtos/auth/signup_dto.dart
@@ -0,0 +1,17 @@
+class SignupDto {
+  final String name;
+  final String email;
+  final String password;
+
+  SignupDto({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'email': email,
+        'password': password,
+      };
+}
