3b22d25e007767882d72270e194f2dcd740bfbed
Yesopi <y.pineros@uniandes.edu.co>
2026-03-20 12:19:59 -0500

    feat(create-listing): enhance CreateListingViewModel with image selection and category management

--- lib/ui/create_listing/create_listing_viewmodel.dart
+++ lib/ui/create_listing/create_listing_viewmodel.dart
@@ -1,48 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:marketplace_flutter_application/data/repositories/category_repository.dart';
 import 'package:marketplace_flutter_application/models/categories/category.dart';
 
 class CreateListingViewModel extends ChangeNotifier {
+  //Categories
   final CategoryRepository _categoryRepository;
-  final List<String> conditions = [
-  'New',
-  'Like New',
-  'Used',
-  ];
+  bool isLoadingCategories = false;
+  String? categoriesErrorMessage;
+  List<Category> categories = [];
+  Category? selectedCategory;
+  // Conditions
+  final List<String> conditions = ['New', 'Like New', 'Used'];
   String selectedCondition = 'Like New';
+  //Images
+  final ImagePicker _imagePicker = ImagePicker();
+  List<XFile> selectedImages = [];
+  final int maxImages = 5;
+  //Basic info
   String title = '';
-String price = '';
-String description = '';
-
-void updateTitle(String value) {
-  title = value;
-  notifyListeners();
-}
-
-void updatePrice(String value) {
-  price = value;
-  notifyListeners();
-}
-
-void updateDescription(String value) {
-  description = value;
-  notifyListeners();
-}
-  void selectCondition(String condition) {
-  selectedCondition = condition;
-  notifyListeners();
-  }
-  CreateListingViewModel({
-    CategoryRepository? categoryRepository,
-  }) : _categoryRepository = categoryRepository ?? CategoryRepository() {
+  String price = '';
+  String description = '';
+  // Constructor
+  CreateListingViewModel({CategoryRepository? categoryRepository})
+    : _categoryRepository = categoryRepository ?? CategoryRepository() {
     loadCategories();
   }
 
-  bool isLoadingCategories = false;
-  String? categoriesErrorMessage;
+  void updateTitle(String value) {
+    title = value;
+    notifyListeners();
+  }
 
-  List<Category> categories = [];
-  Category? selectedCategory;
+  void updatePrice(String value) {
+    price = value;
+    notifyListeners();
+  }
+
+  void updateDescription(String value) {
+    description = value;
+    notifyListeners();
+  }
+
+  void selectCondition(String condition) {
+    selectedCondition = condition;
+    notifyListeners();
+  }
 
   Future<void> loadCategories() async {
     isLoadingCategories = true;
@@ -67,6 +70,28 @@ void updateDescription(String value) {
 
   void selectCategory(Category? category) {
     selectedCategory = category;
+    notifyListeners();
+  }
+  // Images 
+  Future<void> pickImageFromGallery() async {
+    if (selectedImages.length >= maxImages) return;
+
+    final XFile? image = await _imagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (image == null) return;
+
+    if (selectedImages.length < maxImages) {
+      selectedImages = [...selectedImages, image];
+      notifyListeners();
+    }
+  }
+
+  void removeImageAt(int index) {
+    if (index < 0 || index >= selectedImages.length) return;
+
+    selectedImages = List<XFile>.from(selectedImages)..removeAt(index);
     notifyListeners();
   }
 }
