c062f8e943005f5f87634b7b944f150270c419d5
Yesopi <y.pineros@uniandes.edu.co>
2026-03-20 15:09:50 -0500

    feat(listings): refactor ListingApiService to improve code structure and add createListing method

--- lib/data/services/listing_api_service.dart
+++ lib/data/services/listing_api_service.dart
@@ -3,12 +3,13 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:marketplace_flutter_application/config/app_config.dart';
+import 'package:marketplace_flutter_application/data/dtos/listings/create_listing_request.dart';
 import 'package:marketplace_flutter_application/data/dtos/listings/listing_dto.dart';
 
 class ListingApiService {
   final http.Client _httpClient = http.Client();
   final String _baseUrl = AppConfig.apiBaseUrl;
-
+  // Get all listings
   Future<List<ListingDto>> getListings() async {
     final response = await _httpClient.get(
       Uri.parse('$_baseUrl/listings'),
@@ -22,8 +23,28 @@ class ListingApiService {
           .map((json) => ListingDto.fromJson(json as Map<String, dynamic>))
           .toList();
     } else {
-      debugPrint('Failed to load listings: ${response.statusCode} - ${response.body}'); 
+      debugPrint(
+        'Failed to load listings: ${response.statusCode} - ${response.body}',
+      );
       throw Exception('Failed to load listings');
     }
+  }
+
+  // Create a new listing
+  Future<ListingDto> createListing(CreateListingRequest request) async {
+    final response = await _httpClient.post(
+      Uri.parse('$_baseUrl/listings'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(request.toJson()),
+    );
+
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw Exception('Failed to create listing');
+    }
+
+    final Map<String, dynamic> decodedBody =
+        jsonDecode(response.body) as Map<String, dynamic>;
+
+    return ListingDto.fromJson(decodedBody);
   }
 }
