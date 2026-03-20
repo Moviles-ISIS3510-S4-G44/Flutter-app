f1db94ee8031cb56a80bf70bd660e6053a35a181
Yesopi <y.pineros@uniandes.edu.co>
2026-03-19 23:18:07 -0500

    feat(home): add isLoading property to HomeViewModel

--- lib/ui/home/home_viewmodel.dart
+++ lib/ui/home/home_viewmodel.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import '../../models/listings/listing_summary.dart';
 
 class HomeViewModel extends ChangeNotifier {
-
+  bool isLoading = false;
   final List<ListingSummary> featuredListings = [];
   final List<ListingSummary> recentListings = [];
 
