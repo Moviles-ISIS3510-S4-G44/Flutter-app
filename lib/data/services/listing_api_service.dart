a97fb1eb7238d4137da39b80a8e0ebe6e6c9a9a0
Yesopi <y.pineros@uniandes.edu.co>
2026-03-19 22:50:49 -0500

    feat(listing): update getListings method to return a list of ListingDto

--- lib/data/services/listing_api_service.dart
+++ lib/data/services/listing_api_service.dart
@@ -1,11 +1,27 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:marketplace_flutter_application/config/app_config.dart';
+import 'package:marketplace_flutter_application/data/dtos/listing_dto.dart';
 
 class ListingApiService {
   final http.Client _httpClient = http.Client();
   final String _baseUrl = AppConfig.apiBaseUrl;
 
-  Future<http.Response> getListings() async {
-    return _httpClient.get(Uri.parse('$_baseUrl/listings'));
+  Future<List<ListingDto>> getListings() async {
+    final response = await _httpClient.get(
+      Uri.parse('$_baseUrl/listings'),
+      headers: {'Accept': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = jsonDecode(response.body) as List<dynamic>;
+
+      return jsonList
+          .map((json) => ListingDto.fromJson(json as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw Exception('Failed to load listings');
+    }
   }
 }
