57308265d46619c0c15619a48ebd03494afa95c6
Yesopi <y.pineros@uniandes.edu.co>
2026-03-20 19:52:55 -0500

    Add map listing view and view model with routing integration

--- lib/ui/router/app_router.dart
+++ lib/ui/router/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:marketplace_flutter_application/ui/create_listing/create_listing_view.dart';
 import 'package:marketplace_flutter_application/ui/listing-detail/listing_detail_view.dart';
-
+import 'package:marketplace_flutter_application/ui/map_listing/map_listing_view.dart';
 
 import '../home/home_view.dart';
 import '../login/login_view.dart';
@@ -32,6 +32,13 @@ class AppRouter {
         builder: (context, state) {
           final listingId = state.pathParameters['listingId']!;
           return ListingDetailView(listingId: listingId);
+        },
+      ),
+      GoRoute(
+        path: '/listing-map/:listingId',
+        builder: (context, state) {
+          final listingId = state.pathParameters['listingId']!;
+          return MapListingView(listingId: listingId);
         },
       ),
     ],
