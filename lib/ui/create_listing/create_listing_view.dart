617e01e6e483c159c29129bd639acbb62828b764
mariamartinez44 <mc.martinezm1@uniandes.edu.com>
2026-03-20 21:08:50 -0500

    add connectivity view to listing

--- lib/ui/create_listing/create_listing_view.dart
+++ lib/ui/create_listing/create_listing_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:marketplace_flutter_application/ui/connectivity/connectivity_model.dart';
+import 'package:marketplace_flutter_application/ui/connectivity/connectivity_view.dart';
 import 'package:marketplace_flutter_application/ui/create_listing/widgets/listing_basic_details_section.dart';
 import 'package:marketplace_flutter_application/ui/create_listing/widgets/listing_condition_section.dart';
 import 'package:marketplace_flutter_application/ui/create_listing/widgets/listing_description_section.dart';
 import 'package:marketplace_flutter_application/ui/create_listing/widgets/listing_image_picker_section.dart';
 import 'package:marketplace_flutter_application/ui/create_listing/widgets/listing_location_section.dart';
 import 'package:marketplace_flutter_application/ui/create_listing/widgets/listing_submit_section.dart';
+import 'package:provider/provider.dart';
 
 import '../shared/widgets/app_bottom_nav_bar.dart';
 
@@ -25,6 +28,8 @@ class CreateListingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final connectivityModel = context.watch<ConnectivityModel>();
+
     return Scaffold(
       backgroundColor: const Color(0xFFEEF2F7),
       appBar: AppBar(
@@ -42,27 +47,35 @@ class CreateListingView extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              SizedBox(height: 8),
-              ListingImagePickerSection(),
-              SizedBox(height: 24),
-              ListingBasicDetailsSection(),
-              SizedBox(height: 24),
-              ListingConditionSection(),
-              SizedBox(height: 24),
-              ListingLocationSection(),
-              SizedBox(height: 24),
-              ListingDescriptionSection(),
-              SizedBox(height: 24),
-              ListingSubmitSection(),
+        child: Column(
+          children: [
+            if (!connectivityModel.isOnline)
+              const ConnectivityView(),
 
-              SizedBox(height: 16),
-            ],
-          ),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    SizedBox(height: 8),
+                    ListingImagePickerSection(),
+                    SizedBox(height: 24),
+                    ListingBasicDetailsSection(),
+                    SizedBox(height: 24),
+                    ListingConditionSection(),
+                    SizedBox(height: 24),
+                    ListingLocationSection(),
+                    SizedBox(height: 24),
+                    ListingDescriptionSection(),
+                    SizedBox(height: 24),
+                    ListingSubmitSection(),
+                    SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: AppBottomNavBar(
