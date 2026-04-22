c45fc98c7a331c67cc8c5a7100c2124285612b3f
mariamartinez44 <mc.martinezm1@uniandes.edu.com>
2026-04-21 21:06:21 -0500

    add location service as provider

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'package:marketplace_flutter_application/data/repositories/listing_reposi
 import 'package:marketplace_flutter_application/data/services/auth_service.dart';
 import 'package:marketplace_flutter_application/data/services/connectivity_service.dart';
 import 'package:marketplace_flutter_application/data/services/interaction_service.dart';
+import 'package:marketplace_flutter_application/data/services/location_service.dart';
+import 'package:marketplace_flutter_application/data/repositories/location_repository.dart';
 
 import 'package:marketplace_flutter_application/data/storage/auth_token_storage.dart';
 
@@ -67,6 +69,14 @@ class MyApp extends StatelessWidget {
 
         Provider<ListingRepository>(create: (_) => ListingRepository()),
 
+        Provider<LocationService>(create: (_) => LocationService()),
+
+        Provider<LocationRepository>(
+          create: (context) => LocationRepository(
+            locationService: context.read<LocationService>(),
+          ),
+        ),
+
         ChangeNotifierProvider<LoginViewModel>(
           create: (context) => LoginViewModel(
             connectivityService: context.read<ConnectivityService>(),
@@ -83,6 +93,7 @@ class MyApp extends StatelessWidget {
             connectivityService: context.read<ConnectivityService>(),
             listingRepository: context.read<ListingRepository>(),
             interactionRepository: context.read<InteractionRepository>(),
+            locationRepository: context.read<LocationRepository>(),
           ),
         ),
 
