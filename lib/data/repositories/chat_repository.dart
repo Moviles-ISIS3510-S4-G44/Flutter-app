e5b2c5d7b080b00f0bff52de3f6f07a4cd0e6151
Yesopi <y.pineros@uniandes.edu.co>
2026-04-25 01:18:14 -0500

    Implement create conversation functionality in chat repository and service; add CreateConversationRequest model

--- lib/data/repositories/chat_repository.dart
+++ lib/data/repositories/chat_repository.dart
@@ -4,13 +4,22 @@ import 'package:marketplace_flutter_application/models/chats/chat_conversation.d
 class ChatRepository {
   final ChatService _chatService;
 
-  ChatRepository({
-    required ChatService chatService,
-  }) : _chatService = chatService;
+  ChatRepository({required ChatService chatService})
+    : _chatService = chatService;
 
   Future<List<ChatConversation>> getConversations({
     required String accessToken,
   }) {
     return _chatService.getConversations(accessToken: accessToken);
+  }
+
+  Future<ChatConversation> createConversation({
+    required String accessToken,
+    required String listingId,
+  }) {
+    return _chatService.createConversation(
+      accessToken: accessToken,
+      listingId: listingId,
+    );
   }
 }
