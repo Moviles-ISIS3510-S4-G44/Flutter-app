2c487c43fc980115e14d6c7f2c651f6ef233df3c
mariamartinez44 <mc.martinezm1@uniandes.edu.com>
2026-04-25 00:59:12 -0500

    elemento en carrito

---
+++ lib/models/cart/cart_item.dart
@@ -0,0 +1,7 @@
+import 'package:marketplace_flutter_application/models/listings/listing_detail.dart';
+
+class CartItem {
+  final ListingDetail listing;
+
+  const CartItem({required this.listing});
+}
